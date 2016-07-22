a142838150d90ead8e4d0aabed5fc83e3bbeed78
Chenyuan Xie <1147719157xie@gmail.com>
2016-07-22 18:51:12 -0400

    Initial Commit

--- Assignment_5/Program.cs
+++ Assignment_5/Program.cs
@@ -4,6 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/* Author : Chenyuan Xie
+ * Date : July 19th, 2016
+ * Date Modified: Jul 22th, 2016
+ * Description : Assignment 5 File I/O
+ * Version : 1.0 - 
+ *                  Added Documentary
+ *                  Initial release
+ */
 namespace Assignment_5
 {
     class Program
