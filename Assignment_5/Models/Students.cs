a367071b63a657ccca8e4f035b8556fd083f15d7
Chenyuan Xie <1147719157xie@gmail.com>
2016-07-22 21:16:32 -0400

    * Version : 1.1 -  *                  Redo the while loop

---
+++ Assignment_5/Models/Students.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assignment_5.Models
+{
+    public class Students
+    {
+        public int StudentNumber { get; set; }
+        
+
+        public string Name { get; set; }
+
+
+        public string Class { get; set; }
+
+
+        public string Grade { get; set; }
+
+    }
+}
--- Assignment_5/Program.cs
+++ Assignment_5/Program.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Assignment_5.Models;
+using System.IO;
 
 /* Author : Chenyuan Xie
  * Date : July 19th, 2016
  * Date Modified: Jul 22th, 2016
  * Description : Assignment 5 File I/O
- * Version : 0.1 - 
- *                  Change Git Repo
+ * Version : 1.1 - 
+ *                  Redo the while loop
  */
 namespace Assignment_5
 {
@@ -17,6 +19,79 @@ namespace Assignment_5
     {
         static void Main(string[] args)
         {
+
+            try
+            {
+                while (true)
+                {
+                    Console.WriteLine("1-Display Grade       2-Exit");
+                    int choice = int.Parse(Console.ReadLine());
+                    switch (choice)
+                    {
+                        case 1:
+                            const string myfile = "..\\..\\Grade.txt";
+                            List<Students> students = new List<Students>();
+                            const char DELIM = ',';
+                            FileStream inFile = new FileStream(myfile, FileMode.Open, FileAccess.Read);
+                            StreamReader reader = new StreamReader(inFile);
+                            string recordIN;
+                            string[] fields;
+
+                            recordIN = reader.ReadLine();
+
+                            Console.WriteLine("Enter your file to load");
+                            
+
+                                string filename = (Console.ReadLine());
+                            if (filename == "Grade")
+                            {
+
+                                while (recordIN != null)
+                                {
+                                    Students student = new Students();
+                                    fields = recordIN.Split(DELIM);
+                                    student.StudentNumber = Convert.ToInt32(fields[1]);
+                                    student.Name = fields[0];
+                                    student.Class = fields[2];
+                                    student.Grade = fields[3];
+                                    students.Add(student);
+                                
+                                    Console.WriteLine("{0} {1} {2} {3}",
+                                    student.Name,
+                                    student.StudentNumber,
+                                    student.Class,
+                                    student.Grade);
+                                    recordIN = reader.ReadLine();
+                                }
+
+                                }
+                                else
+                                {
+                                    Console.WriteLine("File does not exist");
+                                }
+                                
+                            
+
+                            break;
+                        case 2:
+
+                            return;
+                    }
+                }
+
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine("Error: " + exception.Message);
+            }
+
+
+
+
+
+
+
+            
         }
     }
 }
